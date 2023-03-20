339570200e2b2a0f77bf4307169764c06ca55f25
kimchanyeong <cksdudrrr@gmail.com>
2023-03-21 05:21:47 +0900

    202303210521

--- board.report/src/test/java/board/BoardApplicationTests.java
+++ board.report/src/test/java/board/BoardApplicationTests.java
@@ -1,13 +1,14 @@
 package board;
 
 import org.junit.jupiter.api.Test;
-import org.junit.runner.RunWith;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.mybatis.spring.SqlSessionTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 
-@RunWith(SpringRunner.class)
+//@RunWith(SpringRunner.class)
+@ExtendWith(SpringExtension.class)
 @SpringBootTest
 public class BoardApplicationTests {
 
